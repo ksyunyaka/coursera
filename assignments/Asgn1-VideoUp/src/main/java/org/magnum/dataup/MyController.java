72552a918d344bdcd94265330dbf30976b16fca0
Oksana <Petrova>
2014-08-22 12:00:10 +0300

    Method signatures added

--- assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/MyController.java
+++ assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/MyController.java
@@ -17,10 +17,72 @@
  */
 package org.magnum.dataup;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.magnum.dataup.model.Video;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
+	
+	public static final String DATA_PARAMETER = "data";
+
+	public static final String ID_PARAMETER = "id";
+
+	public static final String VIDEO_SVC_PATH = "/video";
+	
+	public static final String VIDEO_DATA_PATH = VIDEO_SVC_PATH + "/{id}/data";
+	
+	//private List<Video> videos = new ArrayList<Video>();
+    private static final AtomicLong currentId = new AtomicLong(0L);
+
+	private Map<Long,Video> videos = new HashMap<Long, Video>();
+	private List<Video> videoList = new ArrayList<Video>(videos.values());
+
+  	public Video save(Video entity) {
+		checkAndSetId(entity);
+		videos.put(entity.getId(), entity);
+		return entity;
+	}
+
+	private void checkAndSetId(Video entity) {
+		if(entity.getId() == 0){
+			entity.setId(currentId.incrementAndGet());
+		}
+	}
+	//TODO Post method mapping
+	@RequestMapping(value=VIDEO_SVC_PATH, method=RequestMethod.POST)
+	public @ResponseBody Video addVideo(@RequestBody Video v){
+//		v.setId(hashCode());
+		save(v);
+	return v;
+	}
+	
+	//TODO Get method mapping 
+	@RequestMapping(value=VIDEO_SVC_PATH, method=RequestMethod.GET)
+	public @ResponseBody List<Video> getVideos(){
+		return videoList;
+	}
+	
+	//TODO Think about multipart getVideo implementation
+	@RequestMapping (value=VIDEO_DATA_PATH, method=RequestMethod.POST)
+	public @ResponseBody Video addVideoData(@RequestBody Video v){
+		save(v);
+		return v;
+	}
+	
+	@RequestMapping (value=VIDEO_DATA_PATH, method=RequestMethod.GET)
+	public @ResponseBody Map<Long,Video> getVideoData(){
+		return videos;
+	}
 
 
 }
